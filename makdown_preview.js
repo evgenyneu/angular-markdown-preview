5308744ae49f3841ff6b67b8173e75060523631b
Evgenii Neumerzhitckii <evgeny@bikeexchange.com.au>
2013-12-26 11:25:21 +1100

    Move srouce file to root dir

--- makdown_preview.js
+++ makdown_preview.js
@@ -3,25 +3,27 @@
 
   angular.module('evgenyneu.markdown-preview', [])
 
-  .controller('Ctrl', function($scope, $window, $http, $sce) {
-    $scope.md2Html = function() {
-      $scope.html = $window.marked($scope.markdown);
-      $scope.htmlSafe = $sce.trustAsHtml($scope.html);
-    };
+  .controller('Ctrl', ['$scope', '$window', '$http', '$sce',
+    function($scope, $window, $http, $sce) {
+      $scope.md2Html = function() {
+        $scope.html = $window.marked($scope.markdown);
+        $scope.htmlSafe = $sce.trustAsHtml($scope.html);
+      };
 
-    $scope.initFromUrl = function(url) {
-      $http.get(url).success(function(data) {
-        $scope.markdown = data;
-        return $scope.md2Html();
-      });
-    };
+      $scope.initFromUrl = function(url) {
+        $http.get(url).success(function(data) {
+          $scope.markdown = data;
+          return $scope.md2Html();
+        });
+      };
 
-    $scope.initFromText = function(text) {
-      $scope.markdown = text;
-      $scope.md2Html();
-    };
-  })
-  
+      $scope.initFromText = function(text) {
+        $scope.markdown = text;
+        $scope.md2Html();
+      };
+    }
+  ])
+
   .directive('iiMdPreview', function() {
     return {
       template: "<div><textarea class='MdPreview-markdown' name='{{textareaName}}' ng-model='markdown' ng-change='md2Html()'></textarea><div class='MdPreview-html' ng-bind-html='htmlSafe' /></div>",
