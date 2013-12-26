622fe8b81f444c618990924a07e45eb827b1d302
Evgenii Neumerzhitckii <evgeny@bikeexchange.com.au>
2013-12-26 11:47:27 +1100

    Tests

--- karma.conf.js
+++ karma.conf.js
@@ -10,7 +10,7 @@ module.exports = function(config) {
     files: [
       'bower_components/angular/angular.js',
       'bower_components/angular-mocks/angular-mocks.js',
-      'markdown_preview.js',
+      '*.js',
       'test/**/*.coffee'
     ],
 
